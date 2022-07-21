61acfa8d53f5b375a68d937f2aab954f879be1ec
John Constant <johnconstantis@gmail.com>
2022-07-21 13:38:38 +0000

    Cretae get_sales_data function to get sales figures from user

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+'''Import gspread and Google OAuth modules'''
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -15,8 +13,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales figures input from user
+    """
+    print('Please enter sales data from the last market')
+    print('Data should be 6 numbers, separated by commas')
+    print('Example: 1,2,3,4,5,6 \n')
 
-print(data)
+    data_str = input("Enter your sales figures here:")
+    print(f"The data provided is {data_str}")
+
+
+get_sales_data()
